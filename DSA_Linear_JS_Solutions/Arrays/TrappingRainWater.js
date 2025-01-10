5eb3f7289f1c7a15f92530848bc174acab51d2c5
devikavn22 <devika1997eee@gmail.com>
2025-01-11 00:09:35 +0530

    Trapping Rain Water--- Using Two Pointer method------------Updated

--- DSA_Linear_JS_Solutions/Arrays/TrappingRainWater.js
+++ DSA_Linear_JS_Solutions/Arrays/TrappingRainWater.js
@@ -37,3 +37,14 @@ function trap(height) {
 // Example usage:
 console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); // Output: 6
 console.log(trap([4, 2, 0, 3, 2, 5])); // Output: 9
+
+
+/*
+Trapping Rain Water:
+For any position i:
+Water at i=min⁡(leftMax,rightMax)−height[i]
+>> leftMax: The tallest bar to the left of i
+>> rightMax: The tallest bar to the right of i
+>> height[i]: The height of the bar at position i
+
+*/
